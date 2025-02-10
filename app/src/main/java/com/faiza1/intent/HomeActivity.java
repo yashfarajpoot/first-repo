a00ab7288a2fb81f102a7de8c4930675510c2ec2
yashfarajpoot <yashfarajpoot301@gmail.com>
2025-02-10 11:57:17 +0500

    adddatainhomeactivity

--- app/src/main/java/com/faiza1/intent/HomeActivity.java
+++ app/src/main/java/com/faiza1/intent/HomeActivity.java
@@ -2,7 +2,7 @@ package com.faiza1.intent;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,7 +13,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
 
     }
