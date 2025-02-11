ed332fb1669167e2b365c51c8fbcd06cccbbfe0b
mehararooj19 <mehararooj19@gmail.com>
2025-02-11 17:49:51 +0500

    check it

--- app/src/main/java/com/faiza1/intent/MainActivity.java
+++ app/src/main/java/com/faiza1/intent/MainActivity.java
@@ -35,14 +35,6 @@ public class MainActivity extends AppCompatActivity {
         btnSignin = findViewById(R.id.btn_signin);
         tvSignup = findViewById(R.id.tv_signup);
 
-        tvSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //start signup activity
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
 
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,26 +44,26 @@ public class MainActivity extends AppCompatActivity {
                 String password = edtPassword.getText().toString();
 
 
-                if (email.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "please enter email", Toast.LENGTH_LONG).show();
-                    Log.e("onclick", email);
-                    return;
-                }
-                if (password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "please enter password", Toast.LENGTH_LONG).show();
-                    Log.e("onclick", password);
-                    return;
-                }
+                if (!email.isEmpty() && !password.isEmpty()) {
 
-                if (password.length() < 8) {
-                    Toast.makeText(MainActivity.this, "please enter at least 8 character", Toast.LENGTH_LONG).show();
-
-                    return;
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
-                Intent intent = new Intent(MainActivity.this, HomeActivity .class);
-                startActivity(intent);
+                else{
+
+                }
             }
 
             });
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start signup activity
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
