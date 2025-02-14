a7a5d3333001985d0c907520db05331421938b46
mehararooj19 <mehararooj19@gmail.com>
2025-02-14 19:47:00 +0500

    add it

--- app/src/main/java/com/faiza1/intent/SafetyTipsActivity.java
+++ app/src/main/java/com/faiza1/intent/SafetyTipsActivity.java
@@ -8,42 +8,48 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SafetyTipsActivity2 extends AppCompatActivity {
+public class SafetyTipsActivity extends AppCompatActivity {
+
 
     TextView tvHeading;
-    Button btnShareLocation;
-    Button btnEmergencyContact;
+    Button btnLocation;
+    Button btnEmergency;
     Button btnWellLitAreas;
     Button btnAvoidOversharing;
     Button btnAvoidDangerousSituation;
-    Button  btnKeepPhoneCharged;
+    Button  btnPhone;
+    Button btnAlert;
+    Button btnSelf;
+    Button btnDistance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety_tips);
-
-        btnShareLocation = findViewById(R.id.btn_share_location);
-        btnEmergencyContact=findViewById(R.id.btn_emergency_contacts);
+        btnLocation = findViewById(R.id.btn_location);
+        btnEmergency=findViewById(R.id.btn_emergency);
         btnWellLitAreas=findViewById(R.id.btn_well_lit_areas);
-       btnAvoidOversharing=findViewById(R.id.btn_avoid_oversharing);
-       btnAvoidDangerousSituation=findViewById(R.id.btn_avoid_dangerous_situations);
-       btnKeepPhoneCharged=findViewById(R.id.btn_keep_phone_charged);
+        btnAvoidOversharing=findViewById(R.id.btn_avoid_oversharing);
+        btnAvoidDangerousSituation=findViewById(R.id.btn_avoid_dangerous_situation);
+        btnPhone=findViewById(R.id.btn_phone);
+        btnAlert=findViewById(R.id.btn_alert);
+        btnDistance=findViewById(R.id.btn_distance);
+        btnSelf=findViewById(R.id.btn_self);
         tvHeading=findViewById(R.id.tv_heading);
-        btnShareLocation.setOnClickListener(new View.OnClickListener() {
+        btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this, SafetyTipActivity2.class);
                 startActivity(intent);
             }
-
         });
-        btnEmergencyContact.setOnClickListener(new View.OnClickListener() {
+
+        btnEmergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this, SafetyTipActivity2.class);
                 startActivity(intent);
             }
 
@@ -53,7 +59,7 @@ public class SafetyTipsActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this, SafetyTipActivity2.class);
                 startActivity(intent);
             }
 
@@ -61,8 +67,7 @@ public class SafetyTipsActivity2 extends AppCompatActivity {
         btnAvoidOversharing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this,SafetyTipActivity2.class);
                 startActivity(intent);
             }
 
@@ -72,22 +77,22 @@ public class SafetyTipsActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this, SafetyTipActivity2.class);
                 startActivity(intent);
             }
 
         });
 
-        btnKeepPhoneCharged.setOnClickListener(new View.OnClickListener() {
+        btnPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(SafetyTipsActivity2.this, SafetyTip2.class);
+                Intent intent = new Intent(SafetyTipsActivity.this, SafetyTipActivity2.class);
                 startActivity(intent);
             }
 
         });
-
+        
 
 
 
