3077f0a60feb19bf7641ac3290a831e14a6e2365
mehararooj19 <mehararooj19@gmail.com>
2025-02-17 16:13:20 +0500

    add it

---
+++ app/src/main/java/com/faiza1/intent/safteytip/DangerousSituationsActivity.java
@@ -0,0 +1,26 @@
+package com.faiza1.intent.safteytip;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.faiza1.intent.R;
+
+public class DangerousSituationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dangerous_situations);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
