e9e1914c81ff41d66154b80a47f0846f7421f76e
Ehtishamsaith <chaiwithehtisham@gmail.com>
2025-04-09 12:27:18 +0500

    add delete icon

--- app/src/main/java/com/faiza1/intent/NotificationFragment.java
+++ app/src/main/java/com/faiza1/intent/NotificationFragment.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 
 import com.faiza1.intent.model.Notification;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,7 +21,7 @@ import java.util.List;
 public class NotificationFragment extends Fragment {
 
     public NotificationFragment() {
-        // Required empty public constructor
+
     }
 
     @Override
@@ -28,9 +32,9 @@ public class NotificationFragment extends Fragment {
 
         RecyclerView tvNotification = view.findViewById(R.id.tv_notification);
 
-
         List<Notification> notificationList = new ArrayList<>();
 
+<<<<<<< Updated upstream
 
         //load notification from DB
 
@@ -39,17 +43,31 @@ public class NotificationFragment extends Fragment {
 //                .child(FirebaseAuth.getInstance().getUid())
 //                .setValue(null);
 
+=======
+>>>>>>> Stashed changes
         Notification notification = new Notification();
-
         notification.title = "Dangerous";
         notification.body = "I am not safe!";
         notification.date = "10/02/2025 09:43 PM";
 
         notificationList.add(notification);
 
-
         NotificationAdapter adapter = new NotificationAdapter(notificationList);
         tvNotification.setAdapter(adapter);
+
+       ImageView btnDelete = view.findViewById(R.id.btn_delete);
+
+        btnDelete.setOnClickListener(v -> {
+
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Delete Notification")
+                    .setMessage("Are you sure you want to delete this notification?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        Toast.makeText(getContext(), "Notification Deleted", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        });
 
         return view;
     }
