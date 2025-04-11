9ef6eb84204109e748763a20c6cc0aed1ecebe92
Ehtishamsaith <chaiwithehtisham@gmail.com>
2025-04-11 23:48:54 +0500

    changes in notification

--- app/src/main/java/com/faiza1/intent/NotificationFragment.java
+++ app/src/main/java/com/faiza1/intent/NotificationFragment.java
@@ -1,16 +1,16 @@
-package com.faiza1.intent;
-
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 
+import com.faiza1.intent.NotificationAdapter;
+import com.faiza1.intent.R;
 import com.faiza1.intent.model.Notification;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,9 +20,7 @@ import java.util.List;
 
 public class NotificationFragment extends Fragment {
 
-    public NotificationFragment() {
-
-    }
+    public NotificationFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,17 +29,9 @@ public class NotificationFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
         RecyclerView tvNotification = view.findViewById(R.id.tv_notification);
+        tvNotification.setLayoutManager(new LinearLayoutManager(getContext())); // FIXED
 
         List<Notification> notificationList = new ArrayList<>();
-
-
-
-
-
-        //remove all
-//        FirebaseDatabase.getInstance().getReference("Notifications")
-//                .child(FirebaseAuth.getInstance().getUid())
-//                .setValue(null);
 
         Notification notification = new Notification();
         notification.title = "Dangerous";
@@ -53,14 +43,16 @@ public class NotificationFragment extends Fragment {
         NotificationAdapter adapter = new NotificationAdapter(notificationList);
         tvNotification.setAdapter(adapter);
 
-       ImageView btnDelete = view.findViewById(R.id.btn_delete);
+        ImageView btnDelete = view.findViewById(R.id.tv_delete);
 
         btnDelete.setOnClickListener(v -> {
-
-            new AlertDialog.Builder(getContext())
+            new AlertDialog.Builder(requireContext())
                     .setTitle("Delete Notification")
                     .setMessage("Are you sure you want to delete this notification?")
                     .setPositiveButton("Yes", (dialog, which) -> {
+                        // Optional: Delete from Firebase
+                        // FirebaseDatabase.getInstance().getReference("Notifications")
+                        //     .child(FirebaseAuth.getInstance().getUid()).setValue(null);
                         Toast.makeText(getContext(), "Notification Deleted", Toast.LENGTH_SHORT).show();
                     })
                     .setNegativeButton("No", null)
