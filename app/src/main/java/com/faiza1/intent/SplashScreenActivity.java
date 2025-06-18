c21b583b4e7a259ff521a4242df64001e97c5b48
mehararooj19 <mehararooj19@gmail.com>
2025-06-18 12:50:03 +0500

    update admin file

--- app/src/main/java/com/faiza1/intent/SplashScreenActivity.java
+++ app/src/main/java/com/faiza1/intent/SplashScreenActivity.java
@@ -35,7 +35,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
                         if("Admin".equals(user.getRole())){
                             //start admin activity
-                            Intent intent = new Intent(SplashScreenActivity.this, HomeAdminActivity.class);
+                            Intent intent = new Intent(SplashScreenActivity.this, HomeAdmin.class);
                             startActivity(intent);
                         }
                         else{
