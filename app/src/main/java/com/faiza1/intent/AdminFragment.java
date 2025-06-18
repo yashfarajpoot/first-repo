c670d531c65e2bac604291a96c132884ba9e9584
yashfarajpoot <yashfarajpoot301@gmail.com>
2025-06-18 13:46:34 +0500

    adminfragment

---
+++ app/src/main/java/com/faiza1/intent/AdminFragment.java
@@ -0,0 +1,38 @@
+package com.faiza1.intent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AdminFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AdminFragment extends Fragment {
+
+    public AdminFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+   View view = inflater.inflate(R.layout.fragment_admin, container, false);
+   return view;
+    }
+}
--- app/src/main/java/com/faiza1/intent/HomeAdmin.java
+++ app/src/main/java/com/faiza1/intent/HomeAdmin.java
@@ -1,21 +1,39 @@
 package com.faiza1.intent;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeAdmin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home_admin);
-
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.item_userhistory){
+              loadFragment(new AdminFragment());
+                }
+                return true;
+            }
+        });
     }
-}
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction().replace(R.id.fragment_container,new AdminFragment())
+                .commit();
+    }
+    }
